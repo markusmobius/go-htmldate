10655c2cb53be51c5bfb95861010389f10563664
Radhi Fadlillah <m.radhi.f@gmail.com>
2021-06-23 20:05:28 +0700

    Initial unit test for main function

--- core_test.go
+++ core_test.go
@@ -2,9 +2,69 @@ package htmldate
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_HtmlDate(t *testing.T) {
+	// Variables
+	var str, url string
+	var dt time.Time
+	useOriginalDate := Options{UseOriginalDate: true}
+
+	// Helper function
+	format := func(t time.Time) string {
+		return t.Format("2006-01-02")
+	}
+
+	// These pages shouldnt return any date
+	url = "https://www.intel.com/content/www/us/en/legal/terms-of-use.html"
+	dt = extractMockFile(url)
+	assert.True(t, dt.IsZero())
+
+	url = "https://en.support.wordpress.com/"
+	dt = extractMockFile(url)
+	assert.True(t, dt.IsZero())
+
+	str = "<html><body>XYZ</body></html>"
+	dt = extractFromString(str)
+	assert.True(t, dt.IsZero())
+
+	// Handle meta elements
+	str = `<html><head><meta property="dc:created" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta property="dc:created" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str, useOriginalDate)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta http-equiv="date" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str, useOriginalDate)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta name="last-modified" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta property="OG:Updated_Time" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta property="og:updated_time" content="2017-09-01"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-09-01", format(dt))
+
+	str = `<html><head><meta name="created" content="2017-01-09"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-01-09", format(dt))
+
+	str = `<html><head><meta itemprop="copyrightyear" content="2017"/></head><body></body></html>`
+	dt = extractFromString(str)
+	assert.Equal(t, "2017-01-01", format(dt))
+
+}
 
 func Test_compareReference(t *testing.T) {
 	opts := Options{
---
+++ mock_test.go
@@ -0,0 +1,154 @@
+package htmldate
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+// openMockFile is used to open HTML document from specified mock file.
+// Make sure to close the reader later.
+func openMockFile(url string) io.ReadCloser {
+	// Open file
+	path := mapMockFiles[url]
+	path = filepath.Join("test-files", "mock", path)
+
+	f, err := os.Open(path)
+	if err != nil {
+		logrus.Panicln(err)
+	}
+
+	return f
+}
+
+// extractMockFile open then extract content from a mock file.
+func extractMockFile(url string, customOpts ...Options) time.Time {
+	// Open mock file
+	f := openMockFile(url)
+	defer f.Close()
+
+	// Extract
+	opts := Options{URL: url}
+	if len(customOpts) > 0 {
+		opts = mergeOpts(opts, customOpts[0])
+	}
+
+	result, err := FromReader(f, opts)
+	if err != nil {
+		logrus.Panicln(err)
+	}
+
+	return result
+}
+
+func extractFromString(s string, customOpts ...Options) time.Time {
+	opts := Options{}
+	if len(customOpts) > 0 {
+		opts = mergeOpts(opts, customOpts[0])
+	}
+
+	r := strings.NewReader(s)
+	result, err := FromReader(r, opts)
+	if err != nil {
+		logrus.Panicln(err)
+	}
+
+	return result
+}
+
+func mergeOpts(opt1, opt2 Options) Options {
+	opt1.UseOriginalDate = opt1.UseOriginalDate || opt2.UseOriginalDate
+	opt1.UseExtensiveSearch = opt1.UseExtensiveSearch || opt2.UseExtensiveSearch
+
+	if opt2.DateFormat != "" {
+		opt1.DateFormat = opt2.DateFormat
+	}
+
+	if opt2.URL != "" {
+		opt1.URL = opt2.URL
+	}
+
+	if !opt2.MinDate.IsZero() {
+		opt1.MinDate = opt2.MinDate
+	}
+
+	if !opt2.MaxDate.IsZero() {
+		opt1.MaxDate = opt2.MaxDate
+	}
+
+	return opt1
+}
+
+var mapMockFiles = map[string]string{
+	"http://blog.kinra.de/?p=959/":                                              "kinra.de.html",
+	"http://blog.python.org/2016/12/python-360-is-now-available.html":           "blog.python.org.html",
+	"http://blog.todamax.net/2018/midp-emulator-kemulator-und-brick-challenge/": "blog.todamax.net.html",
+	"http://carta.info/der-neue-trend-muss-statt-wunschkoalition/":              "carta.info.html",
+	"https://500px.com/photo/26034451/spring-in-china-by-alexey-kruglov":        "500px.com.spring.html",
+	"https://bayern.de/":                 "bayern.de.html",
+	"https://creativecommons.org/about/": "creativecommons.org.html",
+	"https://die-partei.net/sh/":         "die-partei.net.sh.html",
+	"https://en.blog.wordpress.com/":     "blog.wordpress.com.html",
+	"https://en.support.wordpress.com/":  "support.wordpress.com.html",
+	"https://futurezone.at/digital-life/wie-creativecommons-richtig-genutzt-wird/24.600.504": "futurezone.at.cc.html",
+	"https://github.com/adbar/htmldate": "github.com.html",
+	"https://netzpolitik.org/2016/die-cider-connection-abmahnungen-gegen-nutzer-von-creative-commons-bildern/": "netzpolitik.org.abmahnungen.html",
+	"https://pixabay.com/en/service/terms/":                   "pixabay.com.tos.html",
+	"https://www.austria.info/":                               "austria.info.html",
+	"https://www.befifty.de/home/2017/7/12/unter-uns-montauk": "befifty.montauk.html",
+	"https://www.beltz.de/fachmedien/paedagogik/didacta_2019_in_koeln_19_23_februar/beltz_veranstaltungen_didacta_2016/veranstaltung.html?tx_news_pi1%5Bnews%5D=14392&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Baction%5D=detail&cHash=10b1a32fb5b2b05360bdac257b01c8fa": "beltz.de.didakta.html",
+	"https://www.channelpartner.de/a/sieben-berufe-die-zukunft-haben,3050673": "channelpartner.de.berufe.html",
+	"https://www.creativecommons.at/faircoin-hackathon":                       "creativecommons.at.faircoin.html",
+	"https://www.deutschland.de/en":                                           "deutschland.de.en.html",
+	"https://www.eff.org/files/annual-report/2015/index.html":                 "eff.org.2015.html",
+	"https://www.facebook.com/visitaustria/":                                  "facebook.com.visitaustria.html",
+	"https://www.gnu.org/licenses/gpl-3.0.en.html":                            "gnu.org.gpl.html",
+	"https://www.goodform.ch/blog/schattiges_plaetzchen":                      "goodform.ch.blog.html",
+	"https://www.horizont.net/marketing/kommentare/influencer-marketing-was-sich-nach-dem-vreni-frost-urteil-aendert-und-aendern-muss-172529":                         "horizont.net.html",
+	"https://www.intel.com/content/www/us/en/legal/terms-of-use.html":                                                                                                 "intel.com.tos.html",
+	"https://www.pferde-fuer-unsere-kinder.de/unsere-projekte/":                                                                                                       "pferde.projekte.de.html",
+	"https://www.rosneft.com/business/Upstream/Licensing/":                                                                                                            "rosneft.com.licensing.html",
+	"https://www.scs78.de/news/items/warm-war-es-schoen-war-es.html":                                                                                                  "scs78.de.html",
+	"https://www.tagesausblick.de/Analyse/USA/DOW-Jones-Jahresendrally-ade__601.html":                                                                                 "tagesausblick.de.dow.html",
+	"https://www.transgen.de/aktuell/2687.afrikanische-schweinepest-genome-editing.html":                                                                              "transgen.de.aktuell.html",
+	"https://www.weltwoche.ch/ausgaben/2019-4/artikel/forbes-die-weltwoche-ausgabe-4-2019.html":                                                                       "weltwoche.ch.html",
+	"https://www.wunderweib.de/manuela-reimann-hochzeitsueberraschung-in-bayern-107930.html":                                                                          "wunderweib.html",
+	"http://unexpecteduser.blogspot.de/2011/":                                                                                                                         "unexpecteduser.2011.html",
+	"http://viehbacher.com/de/spezialisierung/internationale-forderungsbeitreibung":                                                                                   "viehbacher.com.forderungsbetreibung.html",
+	"http://www.eza.gv.at/das-ministerium/presse/aussendungen/2018/07/aussenministerin-karin-kneissl-beim-treffen-der-deutschsprachigen-aussenminister-in-luxemburg/": "eza.gv.at.html",
+	"http://www.freundeskreis-videoclips.de/waehlen-sie-car-player-tipps-zur-auswahl-der-besten-car-cd-player/":                                                       "freundeskreis-videoclips.de.html",
+	"http://www.greenpeace.org/international/en/campaigns/forests/asia-pacific/":                                                                                      "greenpeace.org.forests.html",
+	"http://www.heimicke.de/chronik/zahlen-und-daten/":                                                                                                                "heimicke.de.zahlen.html",
+	"http://www.hobby-werkstatt-blog.de/arduino/424-eine-arduino-virtual-wall-fuer-den-irobot-roomba.php":                                                             "hobby-werkstatt-blog.de.roomba.html",
+	"http://www.hundeverein-kreisunna.de/termine.html":                                                                                                                "hundeverein-kreisunna.de.html",
+	"http://www.hundeverein-querfurt.de/index.php?option=com_content&view=article&id=54&Itemid=50":                                                                    "hundeverein-querfurt.de.html",
+	"http://www.jovelstefan.de/2012/05/11/parken-in-paris/":                                                                                                           "jovelstefan.de.parken.html",
+	"http://www.klimawandel-global.de/klimaschutz/energie-sparen/elektromobilitat-der-neue-trend/":                                                                    "klimawandel-global.de.html",
+	"http://www.medef.com/en/content/alternative-dispute-resolution-for-antitrust-damages":                                                                            "medef.fr.dispute.html",
+	"http://www.pbrunst.de/news/2011/12/kein-cyberterrorismus-diesmal/":                                                                                               "pbrunst.de.html",
+	"http://www.stuttgart.de/": "stuttgart.de.html",
+	"https://paris-luttes.info/quand-on-comprend-que-les-grenades-12355":                                                    "paris-luttes.info.html",
+	"https://www.brigitte.de/aktuell/riverdale--so-ehrt-die-serie-luke-perry-in-staffel-vier-11602344.html":                 "brigitte.de.riverdale.html",
+	"https://www.ldt.de/ldtblog/fall-in-love-with-black/":                                                                   "ldt.de.fallinlove.html",
+	"http://www.loldf.org/spip.php?article717":                                                                              "loldf.org.html",
+	"https://www.beltz.de/sachbuch_ratgeber/buecher/produkt_produktdetails/37219-12_wege_zu_guter_pflege.html":              "beltz.de.12wege.html",
+	"https://www.oberstdorf-resort.de/interaktiv/blog/unser-kraeutergarten-wannenkopfhuette.html":                           "oberstdorfresort.de.kraeuter.html",
+	"https://www.wienbadminton.at/news/119843/Come-Together":                                                                "wienbadminton.at.html",
+	"https://blog.wikimedia.org/2018/06/28/interactive-maps-now-in-your-language/":                                          "blog.wikimedia.interactivemaps.html",
+	"https://blogs.mediapart.fr/elba/blog/260619/violences-policieres-bombe-retardement-mediatique":                         "mediapart.fr.violences.html",
+	"https://verfassungsblog.de/the-first-decade/":                                                                          "verfassungsblog.de.decade.html",
+	"https://cric-grenoble.info/infos-locales/article/putsh-en-cours-a-radio-kaleidoscope-1145":                             "cric-grenoble.info.radio.html",
+	"https://www.sebastian-kurz.at/magazin/wasserstoff-als-schluesseltechnologie":                                           "kurz.at.wasserstoff.html",
+	"https://la-bas.org/la-bas-magazine/chroniques/Didier-Porte-souhaite-la-Sante-a-Balkany":                                "la-bas.org.porte.html",
+	"https://exporo.de/wiki/europaeische-zentralbank-ezb/":                                                                  "exporo.de.ezb.html",
+	"https://www.revolutionpermanente.fr/Antonin-Bernanos-en-prison-depuis-pres-de-deux-mois-en-raison-de-son-militantisme": "revolutionpermanente.fr.antonin.html",
+	"http://www.wara-enforcement.org/guinee-un-braconnier-delephant-interpelle-et-condamne-a-la-peine-maximale/":            "wara-enforcement.org.guinee.html",
+	"https://ebene11.com/die-arbeit-mit-fremden-dwg-dateien-in-autocad":                                                     "ebene11.com.autocad.html",
+	"https://www.acredis.com/schoenheitsoperationen/augenlidstraffung/":                                                     "acredis.com.augenlidstraffung.html",
+	"https://www.hertie-school.org/en/debate/detail/content/whats-on-the-cards-for-von-der-leyen/":                          "hertie-school.org.leyen.html",
+	"https://www.adac.de/rund-ums-fahrzeug/tests/kindersicherheit/kindersitztest-2018/":                                     "adac.de.kindersitztest.html",
+}
