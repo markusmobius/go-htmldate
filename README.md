1dafb49d70ff9987ff15274c840382166d30c9bf
Radhi Fadlillah <m.radhi.f@gmail.com>
2023-03-17 20:35:09 +0700

    Add further HTML attributes

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ By default Go-HtmlDate will run in extensive mode. You can disabled it by settin
 
 ## Status
 
-This package is stable enough for use and up to date with the original `htmldate` (commit [3549b91][3]).
+This package is stable enough for use and up to date with the original `htmldate` (commit [be8245c][3]).
 However, since time extraction is a brand new feature which doesn't exist in the original, use it with
 care. So far it works quite nicely on most news sites that I've tried, but it still needs more testing.
 
@@ -64,7 +64,7 @@ Now you can use Trafilatura to extract date of a web page. For basic usage you c
 
 ## Usage as CLI Application
 
-To use CLI, you need to build it from source. Make sure you use `go >= 1.16` then run following commands :
+To use CLI, you need to build it from source. Make sure you use `go >= 1.20` then run following commands :
 
 ```
 go install github.com/markusmobius/go-htmldate/cmd/go-htmldate@master
@@ -155,7 +155,7 @@ Like the original, `go-htmldate` is distributed under the [GNU General Public Li
 [0]: https://github.com/adbar/htmldate
 [1]: https://github.com/adbar
 [2]: https://github.com/adbar/htmldate/tree/v1.1.1
-[3]: https://github.com/adbar/htmldate/commit/3549b91
+[3]: https://github.com/adbar/htmldate/commit/be8245c
 [dcg]: https://dataculturegroup.org
 [ref-badge]: https://pkg.go.dev/badge/github.com/markusmobius/go-htmldate.svg
 [ref-link]: https://pkg.go.dev/github.com/markusmobius/go-htmldate
--- constant.go
+++ constant.go
@@ -142,10 +142,10 @@ var monthNumber = map[string]int{
 }
 
 var dateAttributes = sliceToMap(
-	"article.created", "article_date_original",
+	"article.created", "article_date_original", "article:post_date",
 	"article.published", "article:published_time",
-	"article:publicationdate",
-	"bt:pubdate", "citation_date", "citation_publication_date",
+	"article:publicationdate", "bt:pubdate",
+	"citation_date", "citation_publication_date", "content_create_date",
 	"created", "cxenseparse:recs:publishtime",
 	"date", "date_created", "date_published",
 	"datecreated", "dateposted", "datepublished",
@@ -154,13 +154,16 @@ var dateAttributes = sliceToMap(
 	"dc.date.issued", "dc.date.publication",
 	"dcterms.created", "dcterms.date",
 	"dcterms.issued", "dc:created", "dc:date",
-	"displaydate", "doc_date", "gentime",
+	"displaydate", "doc_date", "field-name-post-date", "gentime",
+	"meta", // too loose?
 	// Open Graph: https://opengraphprotocol.org/
 	"og:published_time", "og:article:published_time",
 	"originalpublicationdate", "parsely-pub-date",
 	"pubdate", "publishdate", "publish_date",
-	"published-date", "publication_date", "rnews:datepublished",
-	"sailthru.date", "shareaholic:article_published_time", "timestamp")
+	"published-date", "publication_date",
+	"rbpubdate", "release_date", "rnews:datepublished",
+	"sailthru.date", "shareaholic:article_published_time", "timestamp",
+	"twt-published-at", "video:release_date", "vr:published_time")
 
 var propertyModified = sliceToMap(
 	"article:modified_time", "datemodified",
