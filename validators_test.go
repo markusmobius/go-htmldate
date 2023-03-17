8359dd4e6a56f8d7c9c80085b8d58aa9d5fe6110
Radhi Fadlillah <m.radhi.f@gmail.com>
2023-03-17 21:38:31 +0700

    Tidy up validator test

--- validators_test.go
+++ validators_test.go
@@ -26,6 +26,10 @@ import (
 )
 
 func Test_validateDate(t *testing.T) {
+	tt := func(y, m, d int) time.Time {
+		return time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC)
+	}
+
 	fnValidateFormat := func(s, format string, customOpts ...Options) bool {
 		dt, err := time.Parse(format, s)
 		if err != nil {
@@ -52,30 +56,25 @@ func Test_validateDate(t *testing.T) {
 	assert.True(t, fnValidate("1998-08-08"))
 	assert.True(t, fnValidate("2001-12-31"))
 	assert.True(t, fnValidate("1995-01-01"))
-	assert.False(t, fnValidate("1994-12-31"))
 	assert.False(t, fnValidate("1992-07-30"))
-	assert.False(t, fnValidate("1901-12-98"))
+	assert.False(t, fnValidate("1901-13-98"))
 	assert.False(t, fnValidate("202-01"))
 	assert.False(t, fnValidateFormat("1922", "2006"))
 	assert.True(t, fnValidateFormat("2004", "2006"))
 
 	// Check max and min date
-	opts := Options{MinDate: time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC)}
+	opts := Options{MinDate: tt(1990, 1, 1)}
 	assert.True(t, fnValidate("1991-01-02", opts))
 
-	opts = Options{MinDate: time.Date(1992, 1, 1, 0, 0, 0, 0, time.UTC)}
+	opts = Options{MinDate: tt(1992, 1, 1)}
 	assert.False(t, fnValidate("1991-01-02", opts))
 
-	opts = Options{MaxDate: time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC)}
+	opts = Options{MaxDate: tt(1990, 1, 1)}
 	assert.False(t, fnValidate("1991-01-02", opts))
 
-	assert.True(t, fnValidate("1991-01-02", Options{
-		MinDate: time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC),
-		MaxDate: time.Date(1995, 1, 1, 0, 0, 0, 0, time.UTC),
-	}))
+	opts = Options{MinDate: tt(1990, 1, 1), MaxDate: tt(1995, 1, 1)}
+	assert.True(t, fnValidate("1991-01-02", opts))
 
-	assert.False(t, fnValidate("1991-01-02", Options{
-		MinDate: time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC),
-		MaxDate: time.Date(1990, 12, 31, 0, 0, 0, 0, time.UTC),
-	}))
+	opts = Options{MinDate: tt(1990, 1, 1), MaxDate: tt(1990, 12, 31)}
+	assert.False(t, fnValidate("1991-01-02", opts))
 }
